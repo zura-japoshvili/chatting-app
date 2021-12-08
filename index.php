96368bbb3bb00303abb221d2dfa4789d5b6d1aa9
zura-japoshvili <73229102+zura-japoshvili@users.noreply.github.com>
2021-12-08 20:07:31 +0400

    update

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chatting App</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
-</head>
+<?php 
+    require_once './views/header.php'
+?>
 <body>
     <div class="wrapper">
         <section class="form signup">
@@ -43,5 +36,6 @@
         </section>
     </div>
     <script src="js/pass-show-hide.js"></script>
+    <script src="js/sign-up.js"></script>
 </body>
 </html>
