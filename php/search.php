c715e89fc396c235ab4635dcaaded6048c877450
zura-japoshvili <73229102+zura-japoshvili@users.noreply.github.com>
2021-12-10 20:22:15 +0400

    up

---
+++ php/search.php
@@ -0,0 +1,34 @@
+<?php 
+    require_once 'config.php';
+    session_start();
+
+    $searchTerm = $_POST['searchTerm'];
+    
+    $statement = $pdo->prepare("SELECT * FROM users WHERE 
+                            fname LIKE '%{$searchTerm}%' OR lname LIKE '%{$searchTerm}%' ");
+    $statement->execute();
+    $data = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    $output = '';
+
+    if(empty($data)){
+        $output .= "No users found related to your search term";
+    }else{
+        foreach($data as $i){
+            if($i['unique_id'] === $_SESSION['unique_id']){
+                continue;
+            }
+            $output .= '<a href="#">
+                        <div class="content">
+                            <img src="'."images/".$i['image'].'" alt="">
+                            <div class="details">
+                                <span>'.$i['fname']. " " .$i['lname'].'</span>
+                                <p>This is test message</p>
+                            </div>
+                        </div>
+                        <div class="status-dot"><i class="fas fa-circle"></i></div>
+                    </a>';
+        }
+    }
+    echo $output;
+?>
