3091bd131921ea4fff3307d2263c20c80d9d7f9d
zura-japoshvili <73229102+zura-japoshvili@users.noreply.github.com>
2021-12-11 20:18:37 +0400

    update

--- php/config.php
+++ php/config.php
@@ -1,4 +1,4 @@
 <?php 
-    $pdo = new PDO('mysql:host=localhost;dbname=chat','root','');
+    $pdo = new PDO('mysql:host=localhost;dbname=heroku_56cd6d9f36ae170','bb625332ab8163','8f4bf382');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
 ?>
