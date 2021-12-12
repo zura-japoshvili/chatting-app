5530129fd6ca1293374f4137f6619ee91d366b90
Zura Japoshvili <73229102+zura-japoshvili@users.noreply.github.com>
2021-12-12 20:55:46 +0400

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,5 +1,5 @@
 <?php 
-    $pdo = new PDO('mysql:host=localhost;dbname=chat','root','');
-    // $pdo = new PDO('mysql:host=eu-cdbr-west-02.cleardb.net;dbname=heroku_56cd6d9f36ae170','bb625332ab8163','8f4bf382');
+    // $pdo = new PDO('mysql:host=localhost;dbname=chat','root','');
+    $pdo = new PDO('mysql:host=eu-cdbr-west-02.cleardb.net;dbname=heroku_56cd6d9f36ae170','bb625332ab8163','8f4bf382');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
 ?>
